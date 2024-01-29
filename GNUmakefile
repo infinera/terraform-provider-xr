b84398e3666ae9b9f641410d701409fec572bed5
lam hoang <lhoang@infinera.com>
2024-01-28 23:48:05 -0800

    updated

--- GNUmakefile
+++ GNUmakefile
@@ -3,7 +3,7 @@ HOSTNAME=infinera.com
 NAMESPACE=infinera
 NAME=xr
 BINARY=terraform-provider-${NAME}
-VERSION=0.0.1
+VERSION :=`git describe --tags --abbrev=0`
 OS_ARCH=linux_amd64
 
 default: install
--- internal/xrcm_pf/auth.go
+++ internal/xrcm_pf/auth.go
@@ -5,42 +5,57 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"strings"
 )
 
 // SignIn - Get a new token for user
 func (c *Client) SignIn() (*AuthResponse, error) {
-	if c.Auth.Username == "" || c.Auth.Password == "" {
-		return nil, fmt.Errorf("define username and password")
-	}
-	rb, err := json.Marshal(c.Auth)
-	if err != nil {
-		return nil, err
-	}
 
-	// req, err := http.NewRequest("POST", fmt.Sprintf("%s/signin", c.HostURL), strings.NewReader(string(rb)))
-	// fmt.Println("Connecting server: ", fmt.Sprintf("%s/oauth/token?client_id=test&audience=test", c.HostURL))
+	fmt.Printf("SignIn: host = %s, user= %s", c.HostURL, c.Auth.Username)
+	if c.Auth.Username == "" || c.Auth.Password == "" {
+		return nil, fmt.Errorf("please specify username and password for IPM server")
+	}
 
 	// TODO - Need to data drive config with self signed TLS vs valid certificates
-
 	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true} // Self signed certificate flag, TODO to remove or data drive it latter
-	req, err := http.NewRequest("GET", fmt.Sprintf("%s/oauth/token?client_id=test&audience=test", c.HostURL), strings.NewReader(string(rb)))
+
+	url := "https://" + c.HostURL +"/realms/xr-cm/protocol/openid-connect/token"
+    method := "POST"
+
+	payload := strings.NewReader("username="+ c.Auth.Username+"&password="+c.Auth.Password+"&grant_type=password&client_secret=xr-web-client&client_id=xr-web-client")
+
+	req, err := http.NewRequest(method, url, payload)
 	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+	res, err := c.HTTPClient.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
 		return nil, err
 	}
 
-	body, err := c.doRequest(req)
+	//str1 := fmt.Sprintf("%s", body)
+	//fmt.Println("body auth" + str1)
+	ar := AuthResponse{}
+	var respmap = make(map[string]interface{})
+	err = json.Unmarshal(body, &respmap)
 	if err != nil {
+		fmt.Printf("Unmarshal failed")
 		return nil, err
 	}
-	str1 := fmt.Sprintf("%s", body)
-	// fmt.Println("body auth", str1)
-	ar := AuthResponse{}
-	var respmap map[string]string
-	err = json.Unmarshal([]byte(str1), &respmap)
-	ar.Token = "Bearer " + respmap["access_token"]
-	// fmt.Println("token", ar.Token)
+	ar.Token = "Bearer " + respmap["access_token"].(string)
 	if err != nil {
 		return nil, err
 	}
